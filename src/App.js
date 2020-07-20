d8d0cf34a960a1819503ddc24045c5c3680a6cfd
yakobus <thiagotnon@gmail.com>
2020-07-20 12:52:46 -0300

    content component built

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Title from './components/Title';
+import POPOSList from './components/POPOSList';
 
 
 function App() {
   return (
     <div className="App">
       <Title />
+      <POPOSList />
     </div>
   );
 }
